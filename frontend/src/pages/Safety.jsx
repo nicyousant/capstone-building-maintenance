b6e2be5f3fa310efebac98967567076fe6fc5e0a
nicyousant <90989304+nicyousant@users.noreply.github.com>
2025-11-24 10:04:17 -0500

    safety page

--- frontend/src/pages/Safety.jsx
+++ frontend/src/pages/Safety.jsx
@@ -2,6 +2,17 @@ export default function Safety() {
     return (
        <>
        <h2>Safety</h2>
+   
+       <section>
+            <h3>Always use the appropriate personal protective equipment (PPE) for the task.</h3>
+              <div className="homeCircle"><img src="hardhat.png" /></div>
+           <div className="homeCircle"><img src="high-vis-vest.png" /></div>
+              <div className="homeCircle"><img src="safety-glasses.png" /></div>
+                <div className="homeCircle"><img src="face-mask.png" /></div>
+                 <div className="homeCircle"><img src="ear-protection.png" /></div>
+                   <div className="homeCircle"><img src="safety-gloves.png" style={{height: "150px", width: "84px"}} /></div>
+                   <div className="homeCircle"><img src="work-boot.png" /></div>
+                   </section>
        </>
     )
 }
